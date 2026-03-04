40d92a51e5e10ae614ae67eab21c98dba2625066
Aby <aby.garcia2554@gmail.com>
2026-03-03 18:54:28 -0600

    firebase setup

--- PauseMindfulSpending/PauseMindfulSpending/App/PauseMindfulSpendingApp.swift
+++ PauseMindfulSpending/PauseMindfulSpending/App/PauseMindfulSpendingApp.swift
@@ -1,7 +1,25 @@
 import SwiftUI
+import FirebaseCore
+
+// Firebase setup
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // Initialize Firebase when the app launches
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct PauseMindfulSpendingApp: App {
+    
+    // Register the AppDelegate so Firebase can initialize
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             RootView()
