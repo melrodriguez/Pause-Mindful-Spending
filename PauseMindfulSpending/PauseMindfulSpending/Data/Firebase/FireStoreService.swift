b68b602c6c8a16182531860334a455366f9da81a
Rodriguez <mar9688@granjeno.cs.utexas.edu>
2026-03-06 11:59:56 -0600

    Added create timer and updated add item logic

--- PauseMindfulSpending/PauseMindfulSpending/Data/Firebase/FireStoreService.swift
+++ PauseMindfulSpending/PauseMindfulSpending/Data/Firebase/FireStoreService.swift
@@ -33,14 +33,17 @@ class FireStoreService {
         parentCollection: String,
         parentId: String,
         subCollection: String,
-        data: [String: Any]
+        data: [String: Any],
+        completion: @escaping (String?) -> Void
     ) {
         // Helper function add a document to any subcollections in a user document
         
-        db.collection(parentCollection)
+        let ref = db.collection(parentCollection)
             .document(parentId)
             .collection(subCollection)
             .addDocument(data: data)
+        
+        completion(ref.documentID)
     }
     
     func fetchDocumentFromSubcollection(
@@ -67,17 +70,47 @@ class FireStoreService {
             }
     }
     
-    func createItem(uid: String, data: [String: Any]) {
-        // Creates an item in the items subcollection
+    func updateDocumentFromSubcollection(
+        parentCollection: String,
+        parentId: String,
+        subCollection: String,
+        subId: String,
+        fieldsToUpdate: [String: Any]
+    ) {
+        // Helper function to update any document in a subcollection. You don't have to
+        // include all the data, just update the field you need
+        
+        db.collection(parentCollection)
+            .document(parentId)
+            .collection(subCollection)
+            .document(subId)
+            .setData(fieldsToUpdate, merge: true)
+    }
+    
+    func createItem(uid: String, data: [String: Any], durationSeconds: Int, completion: @escaping (String?) -> Void) {
+        // Creates an item in the items subcollection and will pass the itemId using the
+        // completion handler
+        
         var updatedData = data
         updatedData["createdAt"] = FieldValue.serverTimestamp()
+
+        self.createTimer(uid: uid, durationSeconds: durationSeconds) {
+            data in
+            if data != nil {
+                updatedData["timerId"] = data
+            }
+        }
         
         self.addDocumentToSubcollection(
             parentCollection: "users",
             parentId: uid,
             subCollection: "items",
             data: updatedData
-        )
+        ) { data in
+            if data != nil {
+                completion(data)
+            }
+        }
     }
     
     func fetchItem(
@@ -99,6 +132,70 @@ class FireStoreService {
             }
             else {
                 completion(nil)
+            }
+        }
+    }
+    
+    func updateItem(uid: String, itemId: String, fieldsToUpdate: [String: Any]) {
+        // Updates contents of an item document. Will update the lastUpdatedAt
+        // timestamp
+        
+        var updatedData = fieldsToUpdate
+        updatedData["lastUpdatedAt"] = FieldValue.serverTimestamp()
+        
+        updateDocumentFromSubcollection(
+            parentCollection: "users",
+            parentId: uid,
+            subCollection: "items",
+            subId: itemId,
+            fieldsToUpdate: updatedData
+        )
+    }
+    
+    func getItemsInCategory(
+        uid: String,
+        category: String,
+        completion: @escaping ([[String: Any]]?) -> Void
+    ) {
+        // Retrieves all the items in a category and stores them as an array of
+        // dictionaries that each contain the item contents. It will pass the contents in
+        // a completion handler. If there are no items with this category then it will
+        // pass nil
+        
+        db.collection("items")
+            .whereField("category", isEqualTo: category)
+            .getDocuments{ snapshot, error in
+                if let docs = snapshot?.documents {
+                    let items = docs.map{ $0.data() }
+                    completion(items)
+                }
+                else {
+                    completion(nil)
+                }
+            }
+    }
+    
+    func createTimer(
+        uid: String,
+        durationSeconds: Int,
+        completion: @escaping (String?) -> Void
+    ) {
+        var data: [String: Any] = [:]
+        var now = Date()
+        
+        data["startDate"] = FieldValue.serverTimestamp()
+        data["status"] = "active"
+        data["createdAt"] = FieldValue.serverTimestamp()
+        data["endDate"] = now.addingTimeInterval(TimeInterval(durationSeconds))
+        
+        self.addDocumentToSubcollection(
+            parentCollection: "users",
+            parentId: uid,
+            subCollection: "timers",
+            data: data
+        ) { data in
+            if data != nil {
+                completion(data)
             }
         }
     }
